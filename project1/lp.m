fe61f00e3c6295274f39d4aba9ef8051ae5ed898
George Velentzas <geovelentzas@gmail.com>
2017-11-19 14:14:49 +0200

    Add Matlab 2015 compatible function

--- project1/lp.m
+++ project1/lp.m
@@ -7,10 +7,9 @@ function x = lp(A, b, tol)
 % The solution is returned in the vector x.
 
 
-% Set the options to be used by the linprog solver
-options = optimoptions('linprog','Algorithm','dual-simplex','Display','none','OptimalityTolerance',tol);
-% I converted the problem to LP by using a vector t=[y1 y2 ... ym x1 x2 ... xm]'
-% where y1=|x1|, y2=|x2|, ... ym=|xm| and minimizing f'out with the new constraints
+% Converted the problem to LP by using a vector t=[y1 y2 ... ym x1 x2 ... xm]'
+% where y1=|x1|, y2=|x2|, ... ym=|xm| and minimizing f't with the new constraints
+
 
 [n,m] = size(A); %just to use the variables
 f = [ones(m,1); zeros(m,1)]; %minimize f't which is equal to ||x||_1
@@ -19,7 +18,14 @@ Aeq = [zeros(n,m) A]; %to formulate A*x = b with the use of t vector
 beq = b;
 A = [-eye(m) eye(m); -eye(m) -eye(m)]; %since yi>=xi and yi>=-xi
 b = zeros(2*m,1);
+
+% for Matlab 2017 use the following 2 lines
+options = optimoptions('linprog','Algorithm','dual-simplex','Display','none','OptimalityTolerance',tol);
 t = linprog(f, A, b, Aeq, beq, lb, [], options);
+
+% for Matlab 2015 use the following 2 lines
+% options = optimoptions('linprog','Algorithm','dual-simplex','Display','none','Tolfun',tol);
+% t = linprog(f, A, b, Aeq, beq, lb, [], [], options);
 
 x  = t(m+1:end,1);
 x = sparse(x);
