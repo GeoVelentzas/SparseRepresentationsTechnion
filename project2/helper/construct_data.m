c7ac597409753317e21a504e01280bb7b2b28085
George Velentzas <geovelentzas@gmail.com>
2017-12-20 19:12:49 +0200

    Remove template comments

--- project2/helper/construct_data.m
+++ project2/helper/construct_data.m
@@ -24,59 +24,49 @@ n = sqrt(n_squared);
 %% generate a Mondrian image
 %  by drawing at random a sparse vector x0 of length m with cardinality k
  
-% TODO: Draw at random the locations of the non-zeros
-% Write your code here... nnz_locs = ????;
+% Draw at random the locations of the non-zeros
 nnz_locs = randperm(m);
 nnz_locs = nnz_locs(1:k);
  
-% TODO: Draw at random the values of the coefficients
-% Write your code here... nnz_vals = ????;
+% Draw at random the values of the coefficients
 nnz_vals = randn(1,k);
  
-% TODO: Create a k-sparse vector x0 of length m given the nnz_locs and nnz_vals
-% Write your code here... x0 = ????;
+% Create a k-sparse vector x0 of length m given the nnz_locs and nnz_vals
 x0=sparse(m,1);
 x0(nnz_locs,1) = nnz_vals;
  
-% TODO: Given A and x0, compute the signal b0
-% Write your code here... b0 = ????;
+% Given A and x0, compute the signal b0
 b0 = A*x0;
 
  
 %% Create the measured data vector b of size n^2
  
-% TODO: Compute the dynamic range
-% Write your code here... dynamic_range = ????;
+% Compute the dynamic range
 dynamic_range = max(b0)-min(b0);
  
 % Create a noise vector
 noise_std = sigma*dynamic_range;
 noise = noise_std*randn(n^2,1);
  
-% TODO: Add noise to the original image
-% Write your code here... b0_noisy = ????;
+% Add noise to the original image
 b0_noisy = b0 + noise;
  
  
 %% Create the sampling matrix C of size (p*n^2 x n^2), 0 < p <= 1
  
-% TODO: Create an identity matrix of size (n^2 x n^2)
-% Write your code here... I = ????;
+% Create an identity matrix of size (n^2 x n^2)
 I = eye(n^2);
  
-% TODO: Draw at random the indices of rows to be kept
-% Write your code here... keep_inds = ????;
+% Draw at random the indices of rows to be kept
 inds = randperm(n^2);
 keep_inds = inds(1:round(p*n^2));
  
-% TODO: Create the sampling matrix C of size (p*n^2 x n^2) by keeping rows
+% Create the sampling matrix C of size (p*n^2 x n^2) by keeping rows
 % from I that correspond to keep_inds
-% Write your code here... C = ????;
 C = sparse(I(keep_inds,:));
 
  
-% TODO: Create a subsampled version of the noisy image
-% Write your code here... b = ????;
+% Create a subsampled version of the noisy image
 b = C*b0_noisy;
  
 end
